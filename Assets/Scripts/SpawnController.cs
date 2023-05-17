800a90832dd807837c30a9c772c246f4c770e3f8
Kuzey SAYGILI <47605619+Blackenedd@users.noreply.github.com>
2023-05-17 22:44:15 +0300

    astroid spawner changed to spawncontroller (adding shooting buff)

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnController : MonoBehaviour
+public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject astroidPrefab;
 
